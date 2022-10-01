b3b207739c46cd8143c94e8cffe7cc36f50ca21a
Dee-pii <premrajdeepika@gmail.com>
2022-10-01 22:38:33 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguenments passed to it
+ * Description: a program that prints the number of arguments passed into it
+ * @argc: arguement count
+ * @argv: array of pointer to arguement strings
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
