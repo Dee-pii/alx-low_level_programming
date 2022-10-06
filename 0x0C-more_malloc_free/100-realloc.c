fbcfdd834ee920d96839e4eccd02c2553fcef02c
Dee-pii <premrajdeepika@gmail.com>
2022-10-06 11:18:37 +0000

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,30 +9,45 @@
  * Return: pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	char *clone, *relloc;
-	unsigned int d;
 
-	if (ptr != NULL)
-	clone = ptr;
-	else
-	{
-		return (malloc(new_size));
-	}
+{
+	unsigned char *np;
+	unsigned int a;
+
 	if (new_size == old_size)
-	return (ptr);
+		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (0);
+		return (NULL);
 	}
-	relloc = malloc(new_size);
-	if (relloc == NULL)
-	return (0);
-	for (d = 0; d < (old_size || d < new_size); d++)
+	if (ptr == NULL)
 	{
-		*(relloc + d) = clone[d];
+		ptr = malloc(new_size * sizeof(void *));
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	np = malloc(new_size * sizeof(char));
+	if (np == NULL)
+		return (NULL);
+	a = 0;
+	if (new_size > old_size)
+	{
+		while (a < old_size)
+		{
+			np[a] = ((char *)ptr)[a];
+			a++;
+		}
+		free(ptr);
+		return (np);
+	}
+/* if new_size < old_size */
+	while (a < new_size)
+	{
+		np[a] = ((char *)ptr)[a];
+		a++;
 	}
 	free(ptr);
-return (relloc);
+	return (np);
 }
