ab692d69022198430b5090ed7fca6f980f743a1d
Dee-pii <premrajdeepika@gmail.com>
2022-10-10 13:55:31 +0000

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print file name
+ * Description: a program that prints the name of the file
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	{
+		printf("%s\n", __BASE_FILE__);
+	}
+	return (0);
+}
