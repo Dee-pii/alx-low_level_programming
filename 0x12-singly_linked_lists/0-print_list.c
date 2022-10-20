d3310ae794c313e53a3165b34137d9a7072ab456
Dee-pii <premrajdeepika@gmail.com>
2022-10-20 12:47:39 +0000

    Task 0 corr

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,18 +8,14 @@
  */
 
 size_t print_list(const list_t *h)
-{
-	size_t new_element;
 
-	new_element = 0;
-	while (h != NULL)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		new_element++;
 	}
-	return (new_element);
+	return (i);
 }
