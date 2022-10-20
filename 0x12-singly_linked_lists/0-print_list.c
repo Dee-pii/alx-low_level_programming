e800471b30d99f92c52744d0d233bdd6de119d6b
Dee-pii <premrajdeepika@gmail.com>
2022-10-20 12:46:12 +0000

    Task 0 corr

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,9 @@
 #include "lists.h"
 /**
  * print_list - a function that prints all the elements
+ * Description: a function that prints all the elements
  * @h: singly linked list.
- * Return: number of elements in the list.
+ * Return: number of elements
  */
 
 size_t print_list(const list_t *h)
